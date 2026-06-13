be9e25a48458b7633209d40e43e12bef2ae760d4
Asvin Thakur <asvinthakur07@gmail.com>
2026-06-13 21:50:03 +0530

    Initial Commit

---
+++ PortifolioPage/PortifolioPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortifolioPageApp.swift
+//  PortifolioPage
+//
+//  Created by Asvin Thakur on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PortifolioPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
